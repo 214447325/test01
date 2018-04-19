cee02e5c75498fb7a323887198005b60529c775e
admin <214447325@qq.com>
2018-04-19 21:48:36 +0800

    添加根据用户的ID查找该用户的信息方法

--- user/views.py
+++ user/views.py
@@ -1,13 +1,15 @@
 # -*-coding:utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators import csrf
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_GET
 from  user.models import User as user
 
 import json
 # Create your views here.
 
-
+@csrf_exempt
+@require_GET
 def selectUserAll(request):
     userlist = []
     list = {}
@@ -19,7 +21,27 @@ def selectUserAll(request):
         list['code'] = '1'
         list['message'] = '查询成功'
         list['data'] = userlist
-    else :
+    else:
         list['code'] = '-1'
         list['message'] = '暂无数据'
+    return HttpResponse(json.dumps(list, ensure_ascii=False), content_type="application/json")
+
+@csrf_exempt
+@require_GET
+def getSelectById(request):
+    list = {}
+    id = request.GET['id']
+    if(len(id) > 0):
+        if(id.isdigit):
+            user_list_info = user.objects.get(id=id)
+            user_list = user.getSelectAll(user_list_info)
+            list['code'] = '1'
+            list['message'] = '查询成功'
+            list['data'] = user_list
+        else:
+            list['code'] = '-1'
+            list['message'] = '参数类型错误'
+    else:
+        list['code'] = '-2'
+        list['message'] = '缺少参数'
     return HttpResponse(json.dumps(list, ensure_ascii=False), content_type="application/json")
